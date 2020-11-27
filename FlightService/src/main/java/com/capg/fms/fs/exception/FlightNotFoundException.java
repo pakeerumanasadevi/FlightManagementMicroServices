4c9161f67f8c6c82a22b2a784614a776cf3bfed8
pakeerumanasadevi <pakkiru.manasa98@gmail.com>
2020-11-27 20:15:58 +0530

    final commit

---
+++ FlightService/src/main/java/com/capg/fms/fs/exception/FlightNotFoundException.java
@@ -0,0 +1,12 @@
+package com.capg.fms.fs.exception;
+
+@SuppressWarnings("serial")
+public class FlightNotFoundException extends RuntimeException {
+	public FlightNotFoundException() {
+		super();
+	}
+
+	public FlightNotFoundException(String message) {
+		super(message);
+	}
+}
---
+++ FlightService/src/main/java/com/capg/fms/fs/exception/FlightServiceExceptions.java
@@ -0,0 +1,16 @@
+package com.capg.fms.fs.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class FlightServiceExceptions {
+
+	@ExceptionHandler(value = { FlightNotFoundException.class })
+	public ResponseEntity<Object> exception(FlightNotFoundException exception) {
+		return new ResponseEntity<>("Flight not found", HttpStatus.NOT_FOUND);
+	}
+
+}
